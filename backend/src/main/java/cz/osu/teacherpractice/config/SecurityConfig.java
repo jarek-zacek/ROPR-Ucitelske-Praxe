7d35f4e875ebbfcf446727032d9c4000e027ccd3
jarek-zacek <jaroslav.zacek@gmail.com>
2022-10-05 20:36:09 +0200

    Disabling CORS for testing purposes.

--- backend/src/main/java/cz/osu/teacherpractice/config/SecurityConfig.java
+++ backend/src/main/java/cz/osu/teacherpractice/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().disable();
         http.csrf().disable();
         http.authorizeRequests();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
